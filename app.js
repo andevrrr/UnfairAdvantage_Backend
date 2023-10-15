c9f0ec1f86d97a9cb9a16704fb725ec6a0b50616
Anton <kucherenko-anton1@mail.ru>
2023-10-15 19:37:58 +0300

    user model

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
+
+const port = process.env.PORT;
+const MONGO_DB_URI = process.env.MONGO_DB_URL;
 
 app.use((req, res) => {
     res.send("Hello World");
 })
 
-app.listen(3000);
+mongoose
+  .connect(MONGO_DB_URI)
+  .then((result) => {
+    console.log("Connected to the database successfully!");
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 
---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  availability: {
+    type: [
+      {
+        weekNumber: {
+          type: Number,
+          required: true,
+        },
+        availableDays: {
+          type: [
+            {
+              day: {
+                type: String,
+                enum: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                required: true,
+              },
+            },
+          ],
+          required: true,
+        },
+      },
+    ],
+    required: true,
+    default: [],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
