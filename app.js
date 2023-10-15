703721304e836ae0153fd537053b00abe7923631
Anton <kucherenko-anton1@mail.ru>
2023-10-15 20:16:55 +0300

    get user availability

--- app.js
+++ app.js
@@ -1,83 +1,86 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
-const User = require('./models/user');
+const User = require("./models/user");
 const app = express();
 
 const port = process.env.PORT;
 const MONGO_DB_URI = process.env.MONGO_DB_URL;
 
 const user1 = new User({
-    username: 'user1',
-    availability: [
+  username: "user1",
+  availability: [
     {
       weekNumber: 1,
       availableDays: [
-        { day: 'Mon' },
-        { day: 'Tue' },
-        { day: 'Wed' },
-        { day: 'Thu' },
-        { day: 'Fri' },
+        { day: "Mon" },
+        { day: "Tue" },
+        { day: "Wed" },
+        { day: "Thu" },
+        { day: "Fri" },
       ],
     },
     {
-        weekNumber: 2,
-        availableDays: [
-          { day: 'Tue' },
-          { day: 'Wed' },
-          { day: 'Thu' },
-          { day: 'Fri' },
-        ],
-      },
-      {
-        weekNumber: 3,
-        availableDays: [
-          { day: 'Wed' },
-          { day: 'Thu' },
-          { day: 'Fri' },
-        ],
-      },
-      {
-        weekNumber: 4,
-        availableDays: [
-          { day: 'Thu' },
-          { day: 'Fri' },
-        ],
-      },
-      {
-        weekNumber: 5,
-        availableDays: [
-          { day: 'Mon' },
-          { day: 'Tue' },
-          { day: 'Wed' }
-        ],
-      },
-      {
-        weekNumber: 6,
-        availableDays: [
-          { day: 'Mon' },
-          { day: 'Tue' },
-          { day: 'Wed' },
-          { day: 'Thu' }
-        ],
-      },
-      {
-        weekNumber: 7,
-        availableDays: [
-          { day: 'Mon' },
-          { day: 'Tue' }
-        ],
-      },
+      weekNumber: 2,
+      availableDays: [
+        { day: "Tue" },
+        { day: "Wed" },
+        { day: "Thu" },
+        { day: "Fri" },
+      ],
+    },
+    {
+      weekNumber: 3,
+      availableDays: [{ day: "Wed" }, { day: "Thu" }, { day: "Fri" }],
+    },
+    {
+      weekNumber: 4,
+      availableDays: [{ day: "Thu" }, { day: "Fri" }],
+    },
+    {
+      weekNumber: 5,
+      availableDays: [{ day: "Mon" }, { day: "Tue" }, { day: "Wed" }],
+    },
+    {
+      weekNumber: 6,
+      availableDays: [
+        { day: "Mon" },
+        { day: "Tue" },
+        { day: "Wed" },
+        { day: "Thu" },
+      ],
+    },
+    {
+      weekNumber: 7,
+      availableDays: [{ day: "Mon" }, { day: "Tue" }],
+    },
   ],
-})
+});
 
 user1.save().then(() => {
-    console.log('User saved');
-  });
+  console.log("User saved");
+});
+
+app.get("/users/:username/availability", async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ availability: user.availability });
+  } catch (error) {
+    console.error("Error fetching user availability:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 app.use((req, res) => {
-    res.send("Hello World");
-})
+  res.send("Hello World");
+});
 
 mongoose
   .connect(MONGO_DB_URI)
@@ -88,5 +91,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-
